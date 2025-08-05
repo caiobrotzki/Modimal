b88ccd20e3516afd65dce268a255c7a5aff62d5b
Caio Brotzki <caio.brotzki@gmail.com>
2025-08-05 15:59:15 -0300

    Add hover for nav using map and array

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -5,6 +5,15 @@ import Heart from "../assets/img/heart.svg";
 import { Link } from "react-router-dom";
 import { Leaf } from "phosphor-react";
 
+const navItems = [
+  "Home",
+  "Collection",
+  "New In",
+  "Modiweek",
+  "Plus Size",
+  "Sustainability",
+];
+
 function Nav() {
   return (
     <div className="w-full h-[60px] fixed top-0 z-50">
@@ -13,38 +22,41 @@ function Nav() {
       </div>
       <div className="flex justify-between items-center bg-white py-3 px-6">
         <div>
-          <div className="flex ">
+          <div className="flex">
             <Link to="/" className="flex items-center">
               <h1 className="text-[30px] font-bold ml-[55px]">modimal</h1>
-              <Leaf className="ml-2 rounded-full bg-[#5A6D57] text-white" size={22}  />
+              <Leaf className="ml-2 rounded-full bg-[#5A6D57] text-white" size={22} />
             </Link>
           </div>
           <p className="text-[10px] text-center ml-20">women clothing</p>
         </div>
         <div>
           <ul className="flex space-x-10 hover:cursor-pointer text-[20px]">
-            <li>Collection</li>
-            <li>New In</li>
-            <li>Modiweek</li>
-            <li>Plus Size</li>
-            <li>Sustainability</li>
+            {navItems.map((item) => (
+              <li
+                key={item}
+                className="hover:text-[#5A6D57] hover:scale-105 transition-all duration-300"
+              >
+                {item}
+              </li>
+            ))}
           </ul>
         </div>
         <div>
           <ul className="flex space-x-6 mr-[20px]">
             <li>
-              <img src={Search} alt="Search" className="w-6 h-6" />
+              <img src={Search} alt="Search" className="w-8 h-8" />
             </li>
             <Link to="/Registre">
               <li>
-                <img src={User} alt="User" className="w-6 h-6" />
+                <img src={User} alt="User" className="w-8 h-8" />
               </li>
             </Link>
             <li>
-              <img src={Heart} alt="Heart" className="w-6 h-6" />
+              <img src={Heart} alt="Heart" className="w-8 h-8" />
             </li>
             <li>
-              <img src={Shopping} alt="Shopping" className="w-6 h-6" />
+              <img src={Shopping} alt="Shopping" className="w-8 h-8" />
             </li>
           </ul>
         </div>
@@ -53,4 +65,4 @@ function Nav() {
   );
 }
 
-export default Nav;
+export default Nav
